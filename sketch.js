e0615ad5c75f940509ab213bae7a2554b32471b2
yugsalecha41 <86595254+yugsalecha41@users.noreply.github.com>
2021-09-03 13:50:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);
+  button = createButton("Click to Blow") 
+  button.position(width/2,height-100) 
+  button.class("blowButton");
+  button.mousepressed(blow);
+  drawSprites();
+}
+
+function blow(){
+  Matter.Body.applyForce(ball.body,{x:0,y:0},{x:0,y:0.5});
+}
